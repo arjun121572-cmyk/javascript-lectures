5b57df1b608f6c32f19e73089f0c5eb81bb66961
Arjun Sharma <arjun131315.com>
2026-01-13 00:23:00 +0530

    Added eventlistener to H1

--- ep61/script.js
+++ ep61/script.js
@@ -1,7 +1,11 @@
+const hi=document.querySelector("h1")
+// hi.onclick= sayHi
+hi.addEventListener('click',sayHi)
+
 function sayHi(){
     console.log("HI")
 
 }
-function hello(){
-    console.log("hi there");
-}
+ function hello(){
+      console.log("hi there");
+ }
