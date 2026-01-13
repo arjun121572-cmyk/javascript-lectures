c77645ece222918de82b245caa9e4af8b65887c8
Arjun Sharma <arjun131315.com>
2026-01-14 02:35:15 +0530

    added files to ep64

---
+++ ep64/script.js
@@ -0,0 +1,19 @@
+const h1 = document.querySelector('h1')
+const card = document.querySelector('.card')
+const container = document.querySelector('.container')
+
+let count = 0
+
+// card.addEventListener('click', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+card.addEventListener('mousedown', (e) => {
+    console.log(e);
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+})
