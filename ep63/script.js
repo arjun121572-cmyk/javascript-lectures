6e7bc2296061985cebc041a1a019ef5415070bd1
Arjun Sharma <arjun131315.com>
2026-01-14 02:09:57 +0530

    Added all keyboard events to js file

--- ep63/script.js
+++ ep63/script.js
@@ -3,3 +3,11 @@ window.addEventListener('keypress',(e)=>{
     console.log("key code:",e.code);
     console.log("value:",e.key);
 })
+// window.addEventListener('keydown',(e)=>{
+//     console.log("key code:",e.code);
+//     console.log("value:",e.key);
+// })
+// window.addEventListener('keyup',(e)=>{
+//     console.log("key code:",e.code);
+//     console.log("value:",e.key);
+// })
