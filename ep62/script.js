db78cbaf7ce8ad9f4654c3550c642076d6820fc8
Arjun Sharma <arjun131315.com>
2026-01-13 17:20:11 +0530

    "Add change event listener to handle input value updates"

--- ep62/script.js
+++ ep62/script.js
@@ -7,10 +7,17 @@ const paragraph=document.querySelector('p')
 //     console.log(' double input clicked');
 // })
 let inputValue
-inputUsername.addEventListener('input', (e)=>{
+// inputUsername.addEventListener('input', (e)=>{
+//     console.log(e.target.value);
+//     inputValue=e.target.value
+
+//     paragraph.innerText=e.target.value
+//     console.log(' input value changed');
+// })
+inputUsername.addEventListener('change', (e)=>{
     console.log(e.target.value);
     inputValue=e.target.value
 
     paragraph.innerText=e.target.value
-    console.log(' double input clicked');
+    console.log(' input value changed');
 })
