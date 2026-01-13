473757adc96020804c85902c25799366f023690b
Arjun Sharma <arjun131315.com>
2026-01-13 20:57:30 +0530

    Handle form submit using FormData and prevent default behavior

--- ep62/script.js
+++ ep62/script.js
@@ -1,12 +1,13 @@
 const inputUsername=document.querySelector('#username')
 const paragraph=document.querySelector('p')
+const form=document.querySelector('form')
 // inputUsername.addEventListener('click', ()=>{
 //     console.log('input clicked');
 // })
 // inputUsername.addEventListener('dblclick', ()=>{
 //     console.log(' double input clicked');
 // })
-let inputValue
+// let inputValue
 // inputUsername.addEventListener('input', (e)=>{
 //     console.log(e.target.value);
 //     inputValue=e.target.value
@@ -21,15 +22,23 @@ let inputValue
 //     paragraph.innerText=e.target.value
 //     console.log(' input value changed');
 // })
-inputUsername.addEventListener('focus', (e)=>{
-    console.log(e);
-    inputValue=e.target.value
+// inputUsername.addEventListener('focus', (e)=>{
+//     console.log(e,type);
+//     inputValue=e.target.value
 
-    paragraph.innerText=e.target.value
-})
-inputUsername.addEventListener('blur', (e)=>{
-    console.log(e);
-    inputValue=e.target.value
+//     paragraph.innerText=e.target.value
+// })
+// form.addEventListener('submit', (e)=>{
+//     e.preventDefault()
+//     console.log(e.formData);
+// })
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const myFormData= new FormData(form)
+    console.log(myFormData);
+    console.log(myFormData.entries());
 
-    paragraph.innerText=e.target.value
+    for (const p of myFormData.entries()){
+        console.log(p);
+    }
 })
